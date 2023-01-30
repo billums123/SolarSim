54a8fd1c16284daee450f5fead516db47cb35a2b
billums123 <wthest123@gmail.com>
2023-01-30 09:39:25 -0700

    add padding to fix navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
 import { Box, AppBar, Toolbar, Typography } from "@mui/material";
 import "../stylesheets/header.css";
+import theme from "../theme";
 
 const Header = () => {
   return (
     <Box component="div">
       <AppBar component="nav" className="header">
         <Toolbar>
-          <Typography variant="h6" component="div">
-            SolarSim
+          <Typography variant="h4" component="div">
+            <strong style={{ color: theme.palette.secondary.main }}>
+              Solar
+            </strong>
+            <strong style={{ color: theme.palette.primary.contrastText }}>
+              Sim
+            </strong>
           </Typography>
         </Toolbar>
       </AppBar>
--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,8 +1,23 @@
 import { Box } from "@mui/material";
 import "../stylesheets/inputs.css";
+import theme from "../theme";
 
 const Inputs = () => {
-  return <Box component="div">Hey</Box>;
+  return (
+    <Box
+      component="div"
+      className="inputs"
+      bgcolor="red"
+      // sx={{ mt: 32, position: "relative" }}
+    >
+      <Box>1</Box>
+      <Box>1</Box>
+      <Box>1</Box>
+      <Box>1</Box>
+      <Box>1</Box>
+      <Box>1</Box>
+    </Box>
+  );
 };
 
 export default Inputs;
